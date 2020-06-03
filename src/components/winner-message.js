7e7997dd93ba30d7ff4ef40548af874d7b269729
Jayden Shaw <jayden.shaw@gmail.com>
2020-06-02 22:39:24 -0600

    App logic completed. Placeholder styling added

---
+++ src/components/winner-message.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class WinnerMessage extends React.Component {
+    render() {
+        return (
+            <div className='win-msg'>
+                <h1>Winner!</h1>
+            </div>
+        );
+    }
+}
+
+export default WinnerMessage;
